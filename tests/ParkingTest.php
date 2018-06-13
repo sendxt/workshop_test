cd9537da061caf546512dab23d08657230b76827
Art <arturas.mateika@nfq.lt>
2018-06-13 19:33:45 +0300

    red

--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -1,29 +1,13 @@
 <?php
 
-declare(strict_types=1);
-/*
- * @copyright C UAB NFQ Technologies
- *
- * This Software is the property of NFQ Technologies
- * and is protected by copyright law – it is NOT Freeware.
- *
- * Any unauthorized use of this software without a valid license key
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- *
- * Contact UAB NFQ Technologies:
- * E-mail: info@nfq.lt
- * http://www.nfq.lt
- */
-
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 
 class ParkingTest extends TestCase
 {
-    public function testTrueReturnsTrue(): void
+    public function testCreate()
     {
-        $this->assertTrue(true);
+        $parking = new Parking();
     }
 }
