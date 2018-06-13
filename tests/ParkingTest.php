1ed920dd4e3dc1c22c0dd17cc93bc5113946c94c
Art <arturas.mateika@nfq.lt>
2018-06-13 19:51:00 +0300

    red

--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -10,18 +10,6 @@ class ParkingTest extends TestCase
     public function testCreate()
     {
         $parking = new Parking();
-        $this->assertInstanceOf(get_class($parking), $parking);
-    }
-
-    public function testGetLenght()
-    {
-        $parking = new Parking();
-        $this->assertEquals('10', $parking->getLength());
-    }
-
-    public function testGetWidth()
-    {
-        $parking = new Parking();
-        $this->assertEquals('200', $parking->getWidth());
+        $this->assertInstanceOf(Parking::class, $parking);
     }
 }
