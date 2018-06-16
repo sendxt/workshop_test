81536c6336f23bfaf70be6a1c1ac53a996412664
Art <arturas.mateika@nfq.lt>
2018-06-16 10:26:37 +0300

    OK

--- src/Model/Car.php
+++ src/Model/Car.php
@@ -4,4 +4,25 @@ namespace Model;
 
 class Car
 {
+    private $size;
+
+    /**
+     * @return int
+     */
+    public function getSize(): int
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param int $size
+     *
+     * @return $this
+     */
+    public function setSize(int $size)
+    {
+        $this->size = $size;
+
+        return $this;
+    }
 }
