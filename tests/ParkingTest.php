f8e1b9880419c806703846956ba6a89afec6b745
Art <arturas.mateika@nfq.lt>
2018-06-16 10:36:06 +0300

    Test fail call undefined

--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -19,7 +19,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Model\Car;
+use Model\Parking;
 use PHPUnit\Framework\TestCase;
 
 class ParkingTest extends TestCase
@@ -28,5 +28,13 @@ class ParkingTest extends TestCase
     {
         $parking = new Parking();
         $this->assertInstanceOf(Parking::class, $parking);
+    }
+
+    public function testGetArea()
+    {
+        $parking = new Parking();
+        $parking->setArea(20);
+
+        $this->assertEquals('20', $parking->getArea());
     }
 }
