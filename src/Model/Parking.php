9cd439fb448d2e433e81dc92fee3235706150a9d
Art <arturas.mateika@nfq.lt>
2018-06-16 10:37:46 +0300

    test OK

--- src/Model/Parking.php
+++ src/Model/Parking.php
@@ -4,4 +4,27 @@ namespace Model;
 
 class Parking
 {
+    private $area;
+
+    /**
+     * @return int
+     */
+    public function getArea(): int
+    {
+        return $this->area;
+    }
+
+    /**
+     * @param int $area
+     *
+     * @return $this
+     */
+    public function setArea(int $area)
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+
+
 }
