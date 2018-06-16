103a3b907eeea3f74a0dfd087f70f8a1ee6a3c70
Art <arturas.mateika@nfq.lt>
2018-06-16 10:18:03 +0300

    fail

--- tests/CarTest.php
+++ tests/CarTest.php
@@ -24,20 +24,9 @@ use PHPUnit\Framework\TestCase;
 
 class CarTest extends TestCase
 {
-    /**
-     * @dataProvider numberProvider
-     */
-    public function testGetNumber($number)
+    public function testCreate()
     {
-        $car = new Car($number);
-        $this->assertEquals($number, $car->getNumber());
-    }
-
-    public function numberProvider()
-    {
-        return [
-            ['DEV 001'],
-            ['DEV 002'],
-        ];
+        $car = new Car();
+        $this->assertInstanceOf(Car::class, $car);
     }
 }
