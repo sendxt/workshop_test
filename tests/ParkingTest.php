8f28716129709eaa71650fa6437e7d477e5a050d
Art <arturas.mateika@nfq.lt>
2018-06-16 10:31:40 +0300

    fail class not found

---
+++ tests/ParkingTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright C UAB NFQ Technologies
+ *
+ * This Software is the property of NFQ Technologies
+ * and is protected by copyright law – it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * Contact UAB NFQ Technologies:
+ * E-mail: info@nfq.lt
+ * http://www.nfq.lt
+ */
+
+namespace Tests;
+
+use Model\Car;
+use PHPUnit\Framework\TestCase;
+
+class ParkingTest extends TestCase
+{
+    public function testCreate()
+    {
+        $parking = new Parking();
+        $this->assertInstanceOf(Parking::class, $parking);
+    }
+}
