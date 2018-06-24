532a7dabf95b94955f92d068fbd626092dd8411b
Art <arturas.mateika@nfq.lt>
2018-06-24 19:58:02 +0300

    red method undefined

--- tests/CarTest.php
+++ tests/CarTest.php
@@ -10,8 +10,20 @@ class CarTest extends TestCase
     public function testGetTitle()
     {
         $car = new Car();
-        $car->setTitle(10);
+        $car->setTitle('BMW');
 
         $this->assertEquals('BMW', $car->getTitle());
+    }
+
+    public function testGetSize()
+    {
+        $car = new Car();
+        $car->setSize(10);
+
+        $car2 = new Car();
+        $car2->setSize(100);
+
+        $this->assertEquals(10, $car->getSize());
+        $this->assertEquals(1000, $car->getSize());
     }
 }
