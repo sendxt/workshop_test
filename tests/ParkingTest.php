51351764e27511dd69430c47899639ed77f3013b
Art <arturas.mateika@nfq.lt>
2018-06-24 19:28:26 +0300

    RED class not found

--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Model\Car;
-use Model\Parking;
 use PHPUnit\Framework\TestCase;
 
 class ParkingTest extends TestCase
@@ -13,24 +11,8 @@ class ParkingTest extends TestCase
     public function testGetArea()
     {
         $parking = new Parking();
-        $parking->setArea(20);
+        $parking->setTitle('Europa');
 
-        $this->assertEquals('20', $parking->getArea());
-    }
-
-    public function testAddCar()
-    {
-        $car = new Car();
-        $car->setSize(30);
-
-        $car2 = new Car();
-        $car2->setSize(18);
-
-        $parking = new Parking();
-        $parking->setArea(50);
-        $parking->addCar($car);
-        $parking->addCar($car2);
-
-        $this->assertCount(1, $parking->getCars());
+        $this->assertEquals('Europa', $parking->getTitle());
     }
 }
