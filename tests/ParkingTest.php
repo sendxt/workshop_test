c8323ced32bd8142ec5d80829704eedd8751d951
Art <arturas.mateika@nfq.lt>
2018-06-24 20:32:49 +0300

    Yellow refactoring

--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -29,16 +29,25 @@ class ParkingTest extends TestCase
         $this->assertEquals($height, $parking->getHeight());
     }
 
-    public function testAddCar()
+    /**
+     * @dataProvider carProvider
+     */
+    public function testAddCar($number, $width, $height)
     {
         $parking = new Parking(200, 200);
-        $car = new Car('GRV020', 20, 20);
-        $car2 = new Car('GRV030', 20, 20);
-
+        $car = new Car($number, $width, $height);
         $parking->addCar($car);
-        $parking->addCar($car2);
 
-        $this->assertCount(2, $parking->getCars());
+        $this->assertEquals($number, $parking->getCars()[0]->getNumber());
+        $this->assertCount(1, $parking->getCars());
+    }
+
+    public function carProvider()
+    {
+        return [
+            ['GRV020', 20, 20],
+            ['GRV030', 20, 20],
+        ];
     }
 
     public function spaceProvider()
